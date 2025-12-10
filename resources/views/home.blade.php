afd3dca938b9263037bb7a557de33a50b9c161db
ViniReiwz <vinireisg@usp.br>
2025-12-10 10:35:26 -0300

    Remoção da autenticação para pagina inicial
    Arquivo routes/web.php modificado para que o usuário não tenha que se autenticar para ver a ágina inicial, mas sim para acessar o restante das features
    Criação do arquivo resources/view/home.blade.php como um apagina de 'recepção' à interface

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+@section('title')
+Equivalência
+@endsection
+
+@section('content')
+    <p style="font-size: 24px;">
+        O projeto Equivalência tem como objetivo informatizar e padronizar o processo de requerimentos de equivalência de disciplinas. Tem como pilares principais as bibliotecas uspdev/workflow e uspdev/forms, que foram construídas com foco na generalização. Logo, o projeto Equivalência pode ser utilizado e adaptado em outros contextos, com vários tipos de requerimentos.
+    </p>
+@endsection
