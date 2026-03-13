a5a7238f4a28a40e06e1a11881f0f0091a866f56
Lucas Lopes <lucaslops12@gmail.com>
2026-03-13 16:59:49 -0300

    Melhora a visualização das submissões para administradores

--- app/Http/Controllers/WorkflowController.php
+++ app/Http/Controllers/WorkflowController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Str;
 use Uspdev\Workflow\Workflow;
+use Uspdev\Forms\Form;
 
 class WorkflowController extends Controller
 {
