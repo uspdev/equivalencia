020cefd160e78ee8805d722d1633c50af2cd639d
Lucas Lopes <lucaslops12@gmail.com>
2026-03-23 16:54:39 -0300

    feat: adiciona campos à tabela de equivalencias na migração

--- database/migrations/2026_03_23_150308_create_equivalencias_table.php
+++ database/migrations/2026_03_23_150308_create_equivalencias_table.php
@@ -12,7 +12,39 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('equivalencias', function (Blueprint $table) {
-           
+            $table->id();
+
+            // CAMPOS USP 
+            $table->string('verdis')->nullable();
+            $table->string('codcur')->nullable();
+            $table->string('codhab')->nullable();
+            
+            // CAMPOS COMPARTILHADOS
+            $table->string('coddis');
+            $table->string('nome_disciplina')->nullable();
+            $table->integer('creditos')->nullable();
+            $table->integer('carga_horaria')->nullable();
+            $table->string('curso')->nullable();
+
+            // DISCIPLINA EXTERNA
+            $table->string('ies')->nullable();
+            $table->integer('ano')->nullable();
+            $table->enum('semestre', [1, 2])->nullable();
+            $table->string('frequencia')->nullable();
+            $table->decimal('nota', 4, 2)->nullable();
+
+            // tipo (requerida ou cursada)
+            $table->enum('tipo', ['requerida', 'cursada'])->nullable();
+
+            // RELACIONAMENTO
+            $table->foreignId('equivalencias_id')
+                ->nullable()
+                ->constrained('equivalencias')
+                ->cascadeOnDelete();
+
+            $table->string('pdf_path')->nullable();
+
+            $table->timestamps();
         });
     }
 
