8f120caf3911ac0787a99f8d5ee940143658063a
Lucas Lopes <lucaslops12@gmail.com>
2026-03-25 14:39:02 -0300

    feat: adiciona opção de menu para 'Equivalências diretas'

--- config/laravel-usp-theme.php
+++ config/laravel-usp-theme.php
@@ -55,6 +55,11 @@ $menu = [
         'url' => 'atendimentos',
         'can' => 'user',
     ],
+    [
+        'text' => 'Equivalências diretas',
+        'url' => 'equivalencias',
+        'can' => 'user',
+    ],
 ];
 
 $right_menu = [
