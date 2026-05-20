0ce6a10fa53f02237d084fb18108e3b6a3ea5788
ViniReiwz <vinireisg@usp.br>
2026-05-20 08:45:26 -0300

    Exibição inicial da rota show
    Exibição primitiva das informações de um requerimento de equivalência.
    Dados apenas como placeholder e interface à ser modificada.

--- resources/views/aproveitamentos/show.blade.php
+++ resources/views/aproveitamentos/show.blade.php
@@ -11,8 +11,35 @@
         </h3>
     </div>
     <div class="card-body card-body-sticky">
-        @foreach ($show_data['cursada'] as $cursada)
-            
+        <div class="card card-header card-header-sticky">
+            <div class="row text-center h2 mb-0 mt-0">
+                <div class="text-info col"><strong>Código</strong></div>
+                <div class="text-warning col"><strong>Nome</strong></div>
+                <div class="text-secondary col"><strong>Semestre</strong></div>
+                <div class="text-danger col"><strong>Ano</strong></div>
+                <div class="col"><strong>Frequência</strong></div>
+                <div class="col"><strong>Nota</strong></div>
+                <div class="col"><strong>Créditos</strong></div>
+                <div class="col"><span class="text-nowrap"><strong>Carga horária</strong></span></div>
+            </div>
+        </div>
+        <hr>
+        @foreach ($show_data['cursadas'] as $cursada)
+            <div class="card card-header">
+                <div class="row text-center h3 mb-0 mt-0 ml-0 mr-0">
+                    <div class="text-info col">{{ $cursada['coddis'] }}</div>
+                    <div class="text-warning col">{{ $cursada['nomdis'] }}</div>
+                    <div class="text-secondary col">{{ $cursada['semestre'] }}°</div>
+                    <div class="text-danger col">{{ $cursada['ano'] }}</div>
+                    <div class="col">{{ $cursada['freq'] }}%</div>
+                    <div class="col">{{ $cursada['nota'] }}</div>
+                    <div class="col">{{ $cursada['creditos'] }}</div>
+                    <div class="col">{{ $cursada['carga_hr'] }}</div>
+                </div>
+            </div>
+            @if (!$loop->last)
+                <br>                
+            @endif
         @endforeach
     </div>
 </div>
