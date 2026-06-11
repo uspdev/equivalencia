5ccd86a58572554cb4295232963422d92dd20412
Vinicius Reis Gonçalves <vinireisg@usp.br>
2026-06-11 17:56:59 -0300

    Remoção da lógica do workflow da aplicação

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\AproveitamentoAutomaticoController;
-use App\Http\Controllers\WorkflowController;
 use App\Http\Controllers\AproveitamentoController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,9 +14,6 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', [WorkflowController::class, 'home'])->name('workflows.index');
-
 Route::middleware(['auth'])->prefix('equivalencias')->group(function () {
     Route::middleware('can:equivalencias')->group(function () {
         Route::get('/', [AproveitamentoAutomaticoController::class, 'index'])
@@ -49,28 +45,6 @@ Route::middleware(['auth'])->prefix('equivalencias')->group(function () {
         Route::get('/req/edit/{group}',[AproveitamentoController::class, 'edit'])->name('equivalencias.req-edit');
         Route::put('/req/edit/{group}',[AproveitamentoController::class, 'update'])->name('equivalencias.req-update');
     });
-
-    Route::get('/createdefinition', [WorkflowController::class, 'createDefinition'])->name('workflows.create-definition');
-    Route::post('/createdefinition', [WorkflowController::class, 'storeDefinition'])->name('workflows.store-definition');
-    Route::get('/listdefinitions', [WorkflowController::class, 'listDefinitions'])->name('workflows.list-definitions');
-    Route::get('/definition/{definition}', [WorkflowController::class, 'showDefinition'])->name('workflows.showDefinition');
-    Route::delete('/definition/{definition}', [WorkflowController::class, 'destroyDefinition'])->name('workflows.destroyDefinition');
-    Route::get('/editdefinition/{definition}', [WorkflowController::class, 'editDefinition'])->name('workflows.editDefinition');
-    Route::post('/updatedefinition/', [WorkflowController::class, 'updateDefinition'])->name('workflows.updateDefinition');
-    Route::get('/exportdefinition/{definitionName}',[WorkflowController::class,'exportDefinition'])->name('workflows.exportDefinition');
-
-    Route::get('/viewcreateobject', [WorkflowController::class, 'viewCreateObject'])->name('workflows.viewCreateObject');
-    Route::get('/createobject/{definitionName}', [WorkflowController::class, 'createObject'])->name('workflows.createObject');
-    Route::post('/createobject/{definitionName}', [WorkflowController::class, 'submitForm']);
-    Route::get('/object/{id}', [WorkflowController::class, 'showObject'])->name('workflows.showObject');
-    Route::get('/object/{id}/form/{transition}', [WorkflowController::class, 'showForm'])->name('workflows.showForm');
-    Route::post('/object/{id}', [WorkflowController::class, 'submitForm'])->name('workflows.showObject');
-    Route::get('/showuserobjects', [WorkflowController::class, 'showUserObjects'])->name('workflows.show-user-objects');
-    Route::post('/apply-transition/{id}', [WorkflowController::class, 'applyTransition'])->name('workflows.applyTransition');
-    Route::delete('/delete-object/{object}', [WorkflowController::class, 'deleteObject'])->name('workflows.delete-object');
-
-    Route::put('/listdefinitions/setuser', [WorkflowController::class, 'setUser'])->name('workflows.setuser');
-    Route::get('/atendimentos', [WorkflowController::class, 'atendimentos'])->name('workflows.atendimentos');
 });
 
 // Permite usar Gate::check('user')na view 404
