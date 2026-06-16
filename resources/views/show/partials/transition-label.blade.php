44666c2f519477c5a37b3fcf4013f0cc094cd51c
ViniReiwz <vinireisg@usp.br>
2026-06-16 16:36:05 -0300

    Lógica movida para a biblioteca

--- resources/views/show/partials/transition-label.blade.php
+++
@@ -1,3 +0,0 @@
-{{-- Exibe o label amigavel da transicao do formulario atual --}}
-{{ $workflowObjectData['workflowDefinition']->definition['transitions'][$formulario['transition']]['label'] ??
-    Str::replace('_', ' ', ucfirst($formulario['transition'])) }}
